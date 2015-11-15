35f74e4eea060407a1cce265272f55945c767604
xsvobo0u <xsvobo0u@stud.fit.vutbr.cz>
2015-11-15 23:07:28 +0100

    pridano zpracovani nekterych vyrazu

--- vyrazy.c
+++ vyrazy.c
@@ -49,3 +49,67 @@ void ZClear(tZasobnik *zasobnik) {
 		ZPop(zasobnik);
 	}
 }
+
+tErrVyr ResolveToken(tToken token, TokenResolved t)
+{
+	switch(token.stav)
+	{
+		case S_ADD:
+					t.operace = PLUS;
+					break;
+
+		case S_DEK:
+					t.operace = DEKREMENT;	//DEKREMENT pridat do tTabOperace
+					break;
+
+		case S_DIV:
+					t.operace = DELENO;
+					break;
+
+		case S_INK:
+					t.operace = INKREMENT;	//INKREMENT pridat do tTabOperace
+					break;
+
+		case S_MEN:
+					t.operace = MENSI;
+					break;
+
+		case S_MUL:
+					t.operace = KRAT;
+					break;
+
+		case S_PRIR:
+					t.operace = PRIRAD;	//PRIRAD pridat do tTabOperace
+					break;
+
+		case S_ROV:
+					t.operace = ROVNASE;
+					break;
+
+		case S_NEROV:
+					t.operace = NEROVNASE;
+					break;
+
+		case S_MENROV:
+					t.operace = MENSIROVNO;
+					break;
+
+		case S_VECROV:
+					t.operace = VETSIROVNO;
+					break;
+
+		case S_SUB:
+					t.operace = MINUS;
+					break;
+
+		case S_VEC:
+					t.operace = VETSI;
+					break;
+
+		case S_VYKR:
+					t.operace = NEGACE;	//NEGACE pridat do tTabOperace
+					break;
+	}
+
+	//return ERR_OK;
+}
