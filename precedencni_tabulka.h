dec2c422c8178f0198ec4d6df414b08ceb5c06bd
Svoboda Jan <xsvobo0u@stud.fit.vutbr.cz>
2015-11-15 19:27:34 +0100

    oprava precedencni tabulky

--- precedencni_tabulka.h
+++ precedencni_tabulka.h
@@ -1,3 +1,6 @@
+#define RADKY   17
+#define SLOUPCE 17
+
 //data do precedencni tabulky
 typedef enum
 {
@@ -27,3 +30,25 @@ typedef enum
 	FUNKCE	   = 25,	//zavolani funkce
 	DOLAR      = 26
 } tTabOperace;
+
+//precedencni tabulka
+const tTab precedencni_tabulka[RADKY][SLOUPCE]
+{
+	{V, V, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
+	{V, V, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
+	{V, V, V, V, V, V, V, V, V, V, M, V, V, V, M, M, V},
+	{V, V, V, V, V, V, V, V, V, V, M, V, V, V, M, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
+	{M, M, M, M, M, M, M, M, M, M, M, R, V, V, R, M, C},
+	{V, V, V, V, V, V, V, V, V, V, C, V, V, V, R, R, V},
+	{M, M, M, M, M, M, M, M, M, M, M, M, V, R, R, M, C},
+	{V, V, V, V, V, V, V, V, V, V, C, M, C, V, R, R, V},
+	{V, V, V, V, V, V, V, V, V, V, R, R, R, R, R, R, V},
+	{R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R},
+	{M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, R},
+};
