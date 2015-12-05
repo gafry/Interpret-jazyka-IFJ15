e36fce347d28f7a7e70bb538435b7d799b1f0779
Svoboda Jan <xsvobo0u@stud.fit.vutbr.cz>
2015-12-05 17:18:35 +0100

    oprava precedencni tabulky

--- precedencni_tabulka.h
+++ precedencni_tabulka.h
@@ -3,8 +3,8 @@
  		pridat chybejici vyrazy do precedencni tabulky
 */
 
-#define RADKY   17
-#define SLOUPCE 17
+#define RADKY   14
+#define SLOUPCE 14
 
 //data do precedencni tabulky
 typedef enum
@@ -29,22 +29,12 @@ typedef enum
 	VETSI      = 9,		// >
 	LKULZAV    = 10,	// (
 	PKULZAV    = 11,	// )
-	LMNOZZAV   = 12,	// {
-	PMNOZZAV   = 13,	// }
-	STREDNIK   = 14,	// ;
-	DEKREMENT  = 15,	// --
-	INKREMENT  = 16,	// ++
-	PRIRAD	   = 17,	// =
-	NEGACE	   = 18,	// !
-	CARKA	   = 19,	// ,
-	KOMENT	   = 20,	// //
-	LKOM	   = 21,	// /*
-	PKOM	   = 22,	// */
-	LPRIR	   = 23,	// <<
-	PPRIR	   = 24,	// >>
-	ID         = 25,	// konstanta nebo nazev promenne
-	FUNKCE	   = 26,	// zavolani funkce
-	DOLAR      = 27		// terminal
+	STREDNIK   = 12,	// ;
+	PRIRAD	   = 13,	// =
+	NEGACE	   = 14,	// !
+	CARKA	   = 15,	// ,
+	ID         = 16,	// konstanta nebo nazev promenne
+	DOLAR      = 17		// terminal
 } tTabOperace;
 
 //chyby pri vyhodnocovani tokenu
@@ -53,29 +43,26 @@ typedef enum tErrVyraz
 	ERR_OK  = 0,	//no problem
 	ERR_LEX = 1,	//lexikalni chyba
 	ERR_SYN = 2,	//syntakticka chyba
-	ERR_SEM = 3	//semanticka chyba
+	ERR_SEM = 3		//semanticka chyba
 } tErrVyr;
 
 //precedencni tabulka
 const tTab precedencni_tabulka[RADKY][SLOUPCE]
 {
-	{V, V, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
-	{V, V, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
-	{V, V, V, V, V, V, V, V, V, V, M, V, V, V, M, M, V},
-	{V, V, V, V, V, V, V, V, V, V, M, V, V, V, M, M, V},
-	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
-	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
-	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
-	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
-	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
-	{M, M, M, M, V, V, V, V, V, V, M, V, V, V, M, M, V},
-	{M, M, M, M, M, M, M, M, M, M, M, R, V, V, R, M, C},
-	{V, V, V, V, V, V, V, V, V, V, C, V, V, V, R, R, V},
-	{M, M, M, M, M, M, M, M, M, M, M, M, V, R, R, M, C},
-	{V, V, V, V, V, V, V, V, V, V, C, M, C, V, R, R, V},
-	{V, V, V, V, V, V, V, V, V, V, R, R, R, R, R, R, V},
-	{R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R, R},
-	{M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, R},
+	{V, V, M, M, V, V, V, V, V, V, M, V, M, V},
+	{V, V, M, M, V, V, V, V, V, V, M, V, M, V},
+	{V, V, V, V, V, V, V, V, V, V, M, V, M, V},
+	{V, V, V, V, V, V, V, V, V, V, M, V, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, M, V},
+	{M, M, M, M, V, V, V, V, V, V, M, V, M, V},
+	{M, M, M, M, M, M, M, M, M, M, M, R, R, C},
+	{V, V, V, V, V, V, V, V, V, V, C, V, R, V},
+	{V, V, V, V, V, V, V, V, V, V, R, R, R, V},
+	{M, M, M, M, M, M, M, M, M, M, M, M, M, R},
 };
 
 //polozka pro vyrazy
